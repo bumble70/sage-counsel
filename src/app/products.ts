e2379bb8882ac9b88e88d2ad70665da9cf79119f
bumble70 <jwmccann1@hotmail.com>
2020-05-04 12:18:59 -0400

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,11 @@
 export const products = [
-  {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+   {
+    name: 'Standardized Test Counseling',
+    description: 'Blah'
   },
   {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Volunteer Counseling',
+    description: 'Be one'
   }
 ];
 
